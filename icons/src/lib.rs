58274832d9d29dc0b3193c6c100c728b2dc0c317
xu_mj <331538695@qq.com>
2024-05-24 14:10:19 +0800

    feat: add voice icon and keyboard icon

--- icons/src/lib.rs
+++ icons/src/lib.rs
@@ -1,5 +1,31 @@
 use yew::prelude::*;
 
+#[function_component(KeyboardIcon)]
+pub fn keyboard_icon() -> Html {
+    html! {
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
+        <g fill="none">
+            <path stroke="#000000" stroke-linejoin="round" stroke-width="4" d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"/>
+            <path fill="#000000" fill-rule="evenodd" d="M15 19a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5m0 8a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5m9-8a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5m0 8a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5m9-8a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5m0 8a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5" clip-rule="evenodd"/>
+            <path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M17 33h14"/>
+        </g>
+    </svg>
+    }
+}
+
+#[function_component(VoiceIcon)]
+pub fn voice_icon() -> Html {
+    html! {
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
+        <g fill="none">
+            <path stroke="#000000" stroke-linejoin="round" stroke-width="4" d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"/>
+            <path fill="#000000" d="M17 25.9a2 2 0 1 0 0-4a2 2 0 0 0 0 4"/>
+            <path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M21.95 28.85A6.978 6.978 0 0 0 24 23.9a6.978 6.978 0 0 0-2.05-4.95m4.95 14.849a13.956 13.956 0 0 0 4.1-9.9c0-3.866-1.567-7.366-4.1-9.899"/>
+        </g>
+    </svg>
+    }
+}
+
 #[function_component(SunIcon)]
 pub fn sun_icon() -> Html {
     html! {
