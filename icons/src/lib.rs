75c78f2e671a4d906ba1819b1f96889220113477
xu_mj <331538695@qq.com>
2024-05-25 11:07:07 +0800

    feat: add voice icon

--- icons/src/lib.rs
+++ icons/src/lib.rs
@@ -1,5 +1,14 @@
 use yew::prelude::*;
 
+#[function_component(VoiceInMsgIcon)]
+pub fn voice_in_msg_icon() -> Html {
+    html! {
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+        <path fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 4v16M8 9v6m12-5v4M4 10v4m12-7v10"/>
+    </svg>
+    }
+}
+
 #[function_component(KeyboardIcon)]
 pub fn keyboard_icon() -> Html {
     html! {
