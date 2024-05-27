f8abe947c49ea0cc9e0d0580d93abb7e2b0fd750
xu_mj <331538695@qq.com>
2024-05-27 18:44:34 +0800

    feat: update voice icon

--- icons/src/lib.rs
+++ icons/src/lib.rs
@@ -12,7 +12,7 @@ pub fn voice_in_msg_icon() -> Html {
 #[function_component(KeyboardIcon)]
 pub fn keyboard_icon() -> Html {
     html! {
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48">
         <g fill="none">
             <path stroke="#000000" stroke-linejoin="round" stroke-width="3" d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"/>
             <path fill="#000000" fill-rule="evenodd" d="M15 19a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5m0 8a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5m9-8a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5m0 8a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5m9-8a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5m0 8a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5" clip-rule="evenodd"/>
@@ -25,7 +25,7 @@ pub fn keyboard_icon() -> Html {
 #[function_component(VoiceIcon)]
 pub fn voice_icon() -> Html {
     html! {
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48">
         <g fill="none">
             <path stroke="#000000" stroke-linejoin="round" stroke-width="3" d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"/>
             <path fill="#000000" d="M17 25.9a2 2 0 1 0 0-4a2 2 0 0 0 0 4"/>
