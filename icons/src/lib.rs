86e892afd5471dc46478be691f61df2fba42431e
xu_mj <331538695@qq.com>
2024-05-28 11:59:09 +0800

    feat: add hangup icon

--- icons/src/lib.rs
+++ icons/src/lib.rs
@@ -1,5 +1,37 @@
 use yew::prelude::*;
 
+#[function_component(HangUpLoadingIcon)]
+pub fn hangup_loading_icon() -> Html {
+    html! {
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+        <circle cx="12" cy="2" r="0" fill="#ffffff">
+            <animate attributeName="r" begin="0" calcMode="spline" dur="1s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/>
+        </circle>
+        <circle cx="12" cy="2" r="0" fill="#ffffff" transform="rotate(45 12 12)">
+            <animate attributeName="r" begin="0.125s" calcMode="spline" dur="1s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/>
+        </circle>
+        <circle cx="12" cy="2" r="0" fill="#ffffff" transform="rotate(90 12 12)">
+            <animate attributeName="r" begin="0.25s" calcMode="spline" dur="1s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/>
+        </circle>
+        <circle cx="12" cy="2" r="0" fill="#ffffff" transform="rotate(135 12 12)">
+            <animate attributeName="r" begin="0.375s" calcMode="spline" dur="1s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/>
+        </circle>
+        <circle cx="12" cy="2" r="0" fill="#ffffff" transform="rotate(180 12 12)">
+            <animate attributeName="r" begin="0.5s" calcMode="spline" dur="1s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/>
+        </circle>
+        <circle cx="12" cy="2" r="0" fill="#ffffff" transform="rotate(225 12 12)">
+            <animate attributeName="r" begin="0.625s" calcMode="spline" dur="1s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/>
+        </circle>
+        <circle cx="12" cy="2" r="0" fill="#ffffff" transform="rotate(270 12 12)">
+            <animate attributeName="r" begin="0.75s" calcMode="spline" dur="1s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/>
+        </circle>
+        <circle cx="12" cy="2" r="0" fill="#ffffff" transform="rotate(315 12 12)">
+            <animate attributeName="r" begin="0.875s" calcMode="spline" dur="1s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/>
+        </circle>
+    </svg>
+    }
+}
+
 #[function_component(ExclamationIcon)]
 pub fn exclamation_icon() -> Html {
     html! {
