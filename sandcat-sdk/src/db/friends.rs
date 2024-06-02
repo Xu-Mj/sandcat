50ac687ea002002e6d232a6aa132042fb0c144e2
xu_mj <331538695@qq.com>
2024-06-02 22:46:14 +0800

    feat: add update avatar and nickname interface

--- sandcat-sdk/src/db/friends.rs
+++ sandcat-sdk/src/db/friends.rs
@@ -10,6 +10,13 @@ use crate::model::friend::Friend;
 pub trait Friends: Debug {
     async fn put_friend(&self, friend: &Friend);
 
+    async fn update_friend_avatar_nickname(
+        &self,
+        id: &str,
+        avatar: AttrValue,
+        nickname: AttrValue,
+    ) -> Result<(), JsValue>;
+
     async fn put_friend_list(&self, friends: &[Friend]);
 
     // async fn get(&self, id: &str) -> Friend;/
@@ -17,6 +24,7 @@ pub trait Friends: Debug {
     async fn get(&self, friend_id: &str) -> Friend;
 
     async fn get_list(&self) -> Result<IndexMap<AttrValue, Friend>, JsValue>;
+
     async fn get_list_by_ids(&self, ids: Vec<String>) -> Result<Vec<Friend>, JsValue>;
 
     async fn delete_friend(&self, id: &str) -> Result<(), JsValue>;
