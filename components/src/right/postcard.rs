1374adc5dff241cfb323c6ab3d4ba779480c0212
xu_mj <331538695@qq.com>
2024-06-07 11:19:35 +0800

    fix: avatar url

--- components/src/right/postcard.rs
+++ components/src/right/postcard.rs
@@ -181,7 +181,7 @@ impl PostCard {
         let avatar = iter
             .map(|v| {
                 html! {
-                    <img class="avatar" src={v.to_owned()} />
+                    <img class="avatar" src={utils::get_avatar_url(v)} />
                 }
             })
             .collect::<Html>();
