7ab77996b59981b5690dd34724ce98ddfb510e40
xu_mj <331538695@qq.com>
2024-06-16 22:16:26 +0800

    fix: avatar url error

--- components/src/right/friend_card.rs
+++ components/src/right/friend_card.rs
@@ -71,7 +71,7 @@ impl Component for FriendCard {
                 onblur={ctx.link().callback(|_| FriendCardMsg::Destroy)}
                 >
                 <div class="friend-card-header">
-                    <img src={&self.friend.avatar} class="friend-card-avatar"/>
+                    <img src={utils::get_avatar_url(&self.friend.avatar)} class="friend-card-avatar"/>
                     <div class="friend-card-info">
                         // <span><b>{&self.friend.remark}</b></span>
                         <span>{tr!(self.i18n, "nickname")}{&self.friend.name}</span>
