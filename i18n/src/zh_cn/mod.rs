b3d8bc7a8604aacbd3b6ad2640d6d8ebe95373cc
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2024-07-28 21:58:41 +0800

    feat: adjust i18n

--- i18n/src/zh_cn/mod.rs
+++ i18n/src/zh_cn/mod.rs
@@ -267,18 +267,18 @@ group_announcement = 群公告
 // WsConnError,
 // WsClosed,
 pub const NOTIFICATION: &str = r#"
-Internal = 内部错误
-UnknownError = 未知错误
-Network = 网络错误
-LocalNotFound = 本地资源不存在
-NotFound = 资源不存在
-ServerError = 服务器错误
-UnAuthorized = 未授权
-BadRequest = 请求错误
-AccountOrPassword = 账号或密码错误
-CodeIsExpired = 验证码已过期
-CodeIsInvalid = 验证码无效
-MsgSendError = 消息发送失败
-WsConnError = WebSocket连接失败
-WsClosed = WebSocket连接已关闭
+Internal = 错误!
+UnknownError = 未知错误!
+Network = 网络错误!
+LocalNotFound = 本地资源不存在!
+NotFound = 资源不存在!
+ServerError = 服务器错误!
+UnAuthorized = 未授权!
+BadRequest = 请求错误!
+AccountOrPassword = 账号或密码错误!
+CodeIsExpired = 验证码已过期!
+CodeIsInvalid = 验证码无效!
+MsgSendError = 消息发送失败!
+WsConnError = WebSocket连接失败!
+WsClosed = WebSocket连接已关闭!
 "#;
